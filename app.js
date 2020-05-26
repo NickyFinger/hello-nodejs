e031a2ce0ae0ed10551844d7ca5a8c43525d9447
Grant.Nicol (remote) <grant.nicol@nicolsoftware.com>
2020-05-25 23:18:44 -0600

    made into express app

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello world!\n');
+// console.log('hello world!\n');
+
+var express = require('express');
+
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!\n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});

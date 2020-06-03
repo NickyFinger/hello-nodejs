72d43a1d24b2b9da0824340ecc5af39bb2dc0b34
Grant.Nicol (remote) <grant.nicol@nicolsoftware.com>
2020-06-03 17:09:00 -0600

    new version

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var express = require('express');
 
 app = express();
 
+var response;
+
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  response = 'This is version 1 of the app.' + '\n';
+  res.send(response);
 });
 
 app.listen(8080, function () {
